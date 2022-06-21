b642f670a2ed73d9b9efc6696d0827680fccf82a
Davi Oliveira <30323152+zBreadz@users.noreply.github.com>
2022-06-21 10:00:50 -0300

    Update API URI

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4ljcdt56mom01yrdswgf7n9/master',
+  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4nl1zg6092t01xo7cks51po/master',
   cache: new InMemoryCache()
 })
