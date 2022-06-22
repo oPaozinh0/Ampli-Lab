6414d64a90d372689c883b2126de3e75913c4926
Davi Oliveira <30323152+zBreadz@users.noreply.github.com>
2022-06-21 22:11:39 -0300

    Class 2 - Visual Structure

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4nl1zg6092t01xo7cks51po/master',
+  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4ng6a8401ky01xyc49h7344/master',
   cache: new InMemoryCache()
 })
